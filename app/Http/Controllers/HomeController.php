9fac3d7d2122ce504c4570da77b550d1c22da7af
Ravali <rk694@njit.edu>
2018-10-31 15:33:10 -0700

    Task: Created a Car model and added Make,Model and Year properties to it.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\User;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,14 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        //return view('home');
+        $users = User::all();
+
+        dd($users->count());
+
+
+        return view('home', ['users' => $users]);
     }
+
+
 }
