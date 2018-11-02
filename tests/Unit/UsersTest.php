03ee9d0d24c3b2b91e13c1b08815386166f6e51f
Ravali <rk694@njit.edu>
2018-11-01 23:39:16 -0700

    Fix: Created a unit test to insert a user into the users table.

---
+++ tests/Unit/UsersTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UsersTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testUsers()
+    {
+        $user=factory (\App\User::class)->make();
+        $this->assertTrue($user->save());
+    }
+}
